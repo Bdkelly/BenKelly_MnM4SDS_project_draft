6a997996bbc86dd16a099aaa1ded7916a99aff6c
bdkelly <benjaminkelly97@gmail.com>
2020-10-07 14:02:26 -0400

    mult

--- starter.py
+++ starter.py
@@ -1,45 +1,39 @@
 import cv2
 import numpy
-import time
 
-def main(tracker,video,num=1):
-    while (video.isOpened()):
+def main(video):
+    track_xl = cv2.MultiTracker_create()
+    container = build(track_xl,video)
+    merge(container,video)
+
+def build(track_xl,video,num=6):
+    tri_dic = {'csrt':cv2.TrackerCSRT_create,'kcf':cv2.TrackerKCF_create,
+                'boosting' : cv2.TrackerBoosting_create,'mil': cv2.TrackerMIL_create}
+    ret, frame = video.read()
+    for i in range(num):
+        cv2.imshow("Frame",frame)
+        rec = cv2.selectROI("Frame",frame)
+        track = tri_dic["csrt"]()
+        track_xl.add(track,frame,rec)
+    return track_xl
+
+def merge(trackers,video):
+    while True:
         ret, frame = video.read()
-        rec = cv2.selectROI("MultiTracker",frame)
-        tracker.init(frame,rec)
         if not ret:
             break
-        (success,box) = tracker.update(frame)
-        if success:
+        (success,boxes) = trackers.update(frame)
+        for box in boxes:
             (x,y,w,h) = [int(a) for a in box]
             cv2.rectangle(frame,(x,y),(x+w,y+h),(100,255,0),2)
         cv2.imshow("Frame",frame)
-        key = cv2.waitKey(0) & 0xFF
+        key = cv2.waitKey(5) & 0xFF
         if key == ord('q'):
             break
-    v.release()
+    video.release()
     cv2.destroyAllWindows()
 
-def justvid(video):
-    while True:
-        ret,frame = video.read()
-        cv2.imshow("Frame",frame)
-        if not ret:
-            break
-        cv2.imshow("Frame",frame)
-        key = cv2.waitKey(0) & 0xFF
-        if key == ord('q'):
-            break
-        cv2.destroyAllWindows()
-    return
-
-
 if __name__ == "__main__":
-    tri_dic = {'csrt':cv2.TrackerCSRT_create,
-               'kcf':cv2.TrackerKCF_create,
-               'boosting' : cv2.TrackerBoosting_create,
-               'mil': cv2.TrackerMIL_create}
-    tracker = tri_dic["csrt"]()
     video = cv2.VideoCapture(r'footage/chipper.mp4') 
-    main(tracker,video)
+    main(video)
     ##justvid(video)
